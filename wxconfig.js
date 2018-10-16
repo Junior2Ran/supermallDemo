9456fb2bd2b0f2007d1151758562932e7807c593
jingse <azure1065@163.com>
2018-10-16 18:53:03 +0800

    47re

--- wxconfig.js
+++ wxconfig.js
@@ -1,9 +1,18 @@
 module.exports = {
   'token': 'junior2ran',
-  'appId': 'wx6d6fd71af24c22c3',
+  
   'encodingAESKey': 'PNuRoIzJBEWYCOR96Zrxe0h1bZCOACEkY9b0gQDeDms',
-  'appSecret': '0094e566a880a093082b7e805f5b1c71',
-  'appKey': 'SWCzyc20161021SWCzyc20161021aaaa',
-  'domainHost': 'http://ymymmall.swczyc.com',
-  'secondaryHost': 'http://ymymmall.swczyc.com/ver'
+ 
+  
+  // 'secondaryHost': 'http://ymymmall.swczyc.com/ver',
+  'secondaryHost': 'http://ymymmall.lvxingbox.cn/ver',
+
+  // 'appId': 'wx6d6fd71af24c22c3',
+  // 'appSecret': '0094e566a880a093082b7e805f5b1c71',
+  // 'appKey': 'SWCzyc20161021SWCzyc20161021aaaa',
+  // 'domainHost': 'http://ymymmall.swczyc.com',
+
+  'appId': 'wxc636210b469ce7dc',
+  'appSecret': 'fc785c46f24105fa3a883e64a01fe2e9',
+  'domainHost': 'http://ymymmall.lvxingbox.cn',
 };
